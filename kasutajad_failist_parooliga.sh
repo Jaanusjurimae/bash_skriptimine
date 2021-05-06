0da2c9eaecf4da8940da933b43d61fd27571f692
Jaanus <jaanus.jurimae@khk.ee>
2021-05-06 14:30:02 -0400

    lisa kasutaja parooliga

---
+++ kasutajad_failist_parooliga.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# kasutajate lisamine
+if [ $# -ne 1 ]; then
+    echo "Kasutusjuhend: $0 failinimi"	
+else
+ failinimi=$1
+ if [ -f $failinimi -a -r $failinimi ]; then
+    echo "fail on korras"
+    for rida in $(cat $failinimi)
+    do
+      # sellega peaks faili sisu olema nähtav, kui fail on kätte saadav ja nüüd echo asemel tuleb kutsuda vajalik skript
+      # echo $nimi
+     kasutajanimi=$(echo "$rida" | cut -f1 -d":")
+ sh lisa_kasutaja.sh $kasutajanimi # sellega me käivitame lisa_kasutaja skripti 
+  echo "$rida" | chpasswd 
+done
+  else
+    echo "probleem failiga $failinimi"
+  fi
+# parameetrite kontrolli lõpp
+fi
